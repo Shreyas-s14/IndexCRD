e31598c3a1e6ea3f071cc678ff5833357c7face1
Shreyas S <shreyas14902@gmail.com>
2024-08-26 04:22:46 +0530

    local + pod create logic

--- internal/controller/index_controller.go
+++ internal/controller/index_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	// "encoding/json"
 	"fmt"
 	// "strings"
 
@@ -37,15 +38,132 @@ import (
 )
 
 // FOR LOCAL TESTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+type ResourceNamespace struct {
+	Namespace string              `json:"namespace,omitempty"`
+	Resources map[string][]string `json:"resources,omitempty"`
+}
+
+type IndexSpec struct {
+	ServiceAccount string                       `json:"serviceAccount,omitempty"`
+	NamespaceMap   map[string]ResourceNamespace `json:"namespaceMap,omitempty"`
+}
+
+// ABOVE FOR LOCAL TESTING: wtihout deploying a cr
 
 // IndexReconciler reconciles a Index object
 type IndexReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	// NamespaceMap map[string]ResourceNamespace // remove later
+	IndexSpecs  map[string]IndexSpec // remove
+	Initialized bool                 // remove later
 }
 
 func (r *IndexReconciler) Init(ctx context.Context) error {
 	fmt.Println("This happens on inittttttttttt")
+	log := log.FromContext(ctx)
+
+	// List all RoleBindings that are not in kube-system, kube-public, or local-path-storage namespaces
+	r.IndexSpecs = make(map[string]IndexSpec)
+
+	// List all RoleBindings that are not in kube-system, kube-public, or local-path-storage namespaces
+	var roleBindings rbacv1.RoleBindingList
+	err := r.List(ctx, &roleBindings)
+	if err != nil {
+		log.Error(err, "Failed to list role bindings")
+		return err
+	}
+
+	// Process RoleBindings to populate IndexSpec instances
+	for _, roleBinding := range roleBindings.Items {
+		if roleBinding.Namespace == "kube-system" || roleBinding.Namespace == "kube-public" || roleBinding.Namespace == "local-path-storage" {
+			continue
+		}
+
+		namespace := roleBinding.Namespace
+
+		for _, subject := range roleBinding.Subjects {
+			if subject.Kind == "ServiceAccount" {
+				serviceAccountName := subject.Name
+
+				// Check if the service account already has an IndexSpec
+				indexSpec, exists := r.IndexSpecs[serviceAccountName]
+				if !exists {
+					// Initialize a new IndexSpec for the service account
+					indexSpec = IndexSpec{
+						ServiceAccount: serviceAccountName,
+						NamespaceMap:   make(map[string]ResourceNamespace),
+					}
+				}
+
+				// Initialize the namespace entry in the NamespaceMap if it doesn't exist
+				if _, exists := indexSpec.NamespaceMap[namespace]; !exists {
+					indexSpec.NamespaceMap[namespace] = ResourceNamespace{
+						Namespace: namespace,
+						Resources: make(map[string][]string),
+					}
+				}
+
+				// List Pods in the current namespace
+				var pods corev1.PodList
+				err := r.List(ctx, &pods, &client.ListOptions{Namespace: namespace})
+				if err != nil {
+					log.Error(err, fmt.Sprintf("Failed to list pods in namespace: %s", namespace))
+					continue
+				}
+
+				// Associate the Pods with the namespace in the IndexSpec
+				for _, pod := range pods.Items {
+					indexSpec.NamespaceMap[namespace].Resources["Pod"] = append(indexSpec.NamespaceMap[namespace].Resources["Pod"], pod.Name)
+				}
+
+				// Update the IndexSpec in the map
+				r.IndexSpecs[serviceAccountName] = indexSpec
+			}
+		}
+	}
+
+	// Log the initialized IndexSpecs for debugging purposes
+	// log.Info("Initialized IndexSpecs", "IndexSpecs", r.IndexSpecs)
+
+	r.Initialized = true
+	log.Info(fmt.Sprintf("%d", len(r.IndexSpecs)))
+	fmt.Println()
+	fmt.Println()
+	fmt.Println(r.IndexSpecs)
+	fmt.Println()
+	fmt.Println()
+	return nil
+}
+
+// Updating pod data
+func (r *IndexReconciler) UpdateServiceAccountResources(ctx context.Context, pod *corev1.Pod) error {
+	log := log.FromContext(ctx)
+	namespace := pod.Namespace
+	podName := pod.Name
+	fmt.Println(len(r.IndexSpecs))
+	for serviceAccount, indexSpec := range r.IndexSpecs {
+		if resourceNamespace, exists := indexSpec.NamespaceMap[namespace]; exists {
+			// Check if the pod is already in the list to avoid duplicates
+			podList := resourceNamespace.Resources["Pod"]
+			podExists := false
+			for _, existingPod := range podList {
+				if existingPod == podName {
+					podExists = true
+					break
+				}
+			}
+
+			if !podExists {
+				resourceNamespace.Resources["Pod"] = append(resourceNamespace.Resources["Pod"], podName)
+				indexSpec.NamespaceMap[namespace] = resourceNamespace
+				r.IndexSpecs[serviceAccount] = indexSpec
+
+				log.Info(fmt.Sprintf("Updated IndexSpec for ServiceAccount: %s with new pod: %s in Namespace: %s", serviceAccount, podName, namespace))
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -65,16 +183,13 @@ func (r *IndexReconciler) Init(ctx context.Context) error {
 func (r *IndexReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
-	// Fetch the Index instance
-	// var index corev1api.Index
-
-	// err := r.Get(ctx, req.NamespacedName, &index)
-	// if err != nil {
-	// 	log.Info("Index resource not found, might have been deleted", "name", req.NamespacedName)
-	// 	return ctrl.Result{}, nil
-	// }
-
-	// Fetch the Pod instance
+	// checking initialization
+	if !r.Initialized {
+		if err := r.Init(ctx); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+// Fetch the Pod instance
 	var pod corev1.Pod
 	err := r.Get(ctx, req.NamespacedName, &pod)
 
@@ -92,66 +207,37 @@ func (r *IndexReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	if pod.ObjectMeta.DeletionTimestamp != nil || pod.ObjectMeta.Generation > 1 {
 		return ctrl.Result{}, nil
 	}
-	// log.Info("yay Pod created in namespace", "namespace", &pod.Namespace, "podName", &pod.Name)
-	// log.V(1).Info("yay Pod created", "namespace", pod.GetNamespace(), "podName", pod.GetName())
+	r.UpdateServiceAccountResources(ctx, &pod)
 	log.Info(fmt.Sprintf("Yay Pod created in %s called %s", pod.Namespace, pod.Name))
+	
 
-	// Roles:
-	// var clusterRoles rbacv1.ClusterRoleList
-	// err = r.List(ctx, &clusterRoles)
-	// fmt.Println(err)
+	fmt.Println(r.IndexSpecs)
+
+	//RoleBindings:
+	// var RoleBindings rbacv1.RoleBindingList
+	// err = r.List(ctx, &RoleBindings)
 	// if err != nil {
-	// 	fmt.Println("Error listing roles:", err)
+	// 	log.Error(err, "Error Listing RoleBindings")
 	// } else {
-	// 	fmt.Println("ClusterRoles found:", len(clusterRoles.Items))
-	// 	for _, clusterRole := range clusterRoles.Items {
-	// 		if !strings.HasPrefix(clusterRole.Name, "system:") && !strings.HasPrefix(clusterRole.Name, "kubeadm:") {
-	// 			fmt.Println("ClusterRole: ", clusterRole.Name)
-	// 			// log.Info(clusterRole.)
+	// 	for _, rb := range RoleBindings.Items {
+	// 		// fmt.Println("Rolebindings : ", rb.Name, "Namespace: ", rb.Namespace)
+	// 		// fmt.Println(rb.Subjects)
+	// 		// fmt.Println(rb.RoleRef.Name)
+	// 		for _, sa := range rb.Subjects {
+	// 			fmt.Println(sa.Kind, " : ", sa.Name)
 	// 		}
 	// 	}
 	// }
-
-	//RoleBindings:
-	var RoleBindings rbacv1.RoleBindingList
-	err = r.List(ctx, &RoleBindings)
-	if err != nil {
-		log.Error(err, "Error Listing RoleBindings")
-	} else {
-		for _, rb := range RoleBindings.Items {
-			// fmt.Println("Rolebindings : ", rb.Name, "Namespace: ", rb.Namespace)
-			// fmt.Println(rb.Subjects)
-			// fmt.Println(rb.RoleRef.Name)
-			for _, sa := range rb.Subjects {
-				fmt.Println(sa.Kind, " : ", sa.Name)
-			}
-		}
-	}
-
-	var serviceAccounts corev1.ServiceAccountList
-	err = r.List(ctx, &serviceAccounts)
-	if err != nil {
-		log.Error(err, "Failed to list service accounts")
-		return ctrl.Result{}, err
-	}
-
-	// Filter out ServiceAccounts that are in "kube-system" or "local-path-storage" namespaces
-	for _, sa := range serviceAccounts.Items {
-		if sa.Namespace != "kube-system" && sa.Namespace != "local-path-storage" {
-			// log.Info(fmt.Sprintf("ServiceAccount: %s, Namespace: %s", sa.Name, sa.Namespace))
-			fmt.Println("ServiceAcc: ", sa.Name, "Namespace: ", sa.Namespace)
-		}
-	}
 	return ctrl.Result{}, nil
 	// TODO(user): your logic here
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *IndexReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	err := r.Init(context.Background())
-	if err != nil {
-		return err
-	}
+	// err := r.Init(context.Background())
+	// if err != nil {
+	// 	return err
+	// }
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&corev1api.Index{}).
 		Watches(
